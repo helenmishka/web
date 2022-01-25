f226a26924c5340ad8a29281181240ae3194cd58
mobosinfo <mobosinfo@gmail.com>
2022-01-25 15:32:15 +0300

    ok

--- university/api.py
+++ university/api.py
@@ -9,7 +9,7 @@ from flask_login import login_user
 from flask_sqlalchemy import SQLAlchemy
 
 from university import app, api
-from university.models import User, Contracts, Workers, Customers
+from university.models import User, Contracts, Workers, Customers, Pay
 
 
 class Users:
@@ -20,7 +20,7 @@ class Users:
     def sign_up(self):
             if self.login and self.password:
                 user = User.query.filter_by(sign_login=self.login).first()
-                print(check_password_hash(user.sign_password, str(self.password)))
+
                 if user and check_password_hash(user.sign_password, self.password):
                     if user.sign_role == 1:
                         global username
@@ -76,8 +76,45 @@ class Contract:
         return rec
 
 
+class AddPay:
+    def __init__(self, account_number, payment_method, payment_date, number_contract):
+        self.account_number = account_number
+        self.payment_method = payment_method
+        self.payment_date = payment_date
+        self.number_contract = number_contract
+
+    def add_pay(self):
+        new_pay = Pay(accountnumber = self.account_number, paymentmethod = self.payment_method, paymentdate = self.payment_date, numbercontract = self.number_contract)
+        try:
+            app.session.add(new_pay)
+            app.session.commit()
+        except:
+            return "Произошла ошибка!"
+
+class PayAPI(Resource):
+
+    @swag_from("pay_json_post.yml")
+    def post(self):
+        account_number = request.args.get("pay_accountnumber")
+        payment_method = request.args.get("pay_paymentmethod")
+        payment_date = request.args.get("pay_paymentdate")
+        number_contract = request.args.get("pay_numbercontract")
+        pay = AddPay(account_number, payment_method, payment_date, number_contract)
+        pay.add_pay()
+        return jsonify({
+            "accountnumber": account_number,
+            "paymentmethod": payment_method,
+            "paymentdate": payment_date,
+            "numbercontract": number_contract
+        })
+
+    @swag_from("pay_json_get.yml")
+    def get(self):
+        pay = Pay.query.all()
+
 ## Api resource routing
 api.add_resource(UserAPI, '/user')
+api.add_resource(PayAPI, '/pay')
 
 if __name__ == "__main__":
     app.run(debug=True)
