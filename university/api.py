7cf668cb22cf4fdbf6bde5cfd0444a00571bfa95
EMishina <030101-@mai.ru>
2022-01-27 01:34:37 +0300

    -

--- university/api.py
+++ university/api.py
@@ -9,8 +9,8 @@ import sqlalchemy
 from flask_login import login_user
 from flask_sqlalchemy import SQLAlchemy
 
-from university import app, api
-from university.models import User, Contracts, Workers, Customers, Pay
+from university import app, api, db
+from university.models import User, Contracts, Workers, Customers, Pay, Directions
 
 
 class Users:
@@ -21,33 +21,41 @@ class Users:
     def sign_up(self):
         if self.login and self.password:
             user = User.query.filter_by(sign_login=self.login).first()
-            print(check_password_hash(user.sign_password, str(self.password)))
             if user and check_password_hash(user.sign_password, self.password):
                 if user.sign_role == 1:
                     global username
                     username = self.login
-                    return 'user_space'
+                    return jsonify({'space': 'user'})
                 elif user.sign_role == 2:
-                    return 'operator_space'
+                    return jsonify({'space': 'operator'})
                 elif user.sign_role == 3:
-                    return 'admin_space'
+                    return jsonify({'space': 'admin'})
             else:
-                return jsonify('Неправильный логин или пароль!')
+                return jsonify({'error': 'Неправильный логин или пароль!'})
         else:
-            return 'Нет логина или пароля!'
+            return jsonify({'error': 'Нет логина или пароля!'})
 
     def registration(self, password2, role):
         if not (self.login or self.password or password2):
-            return 'Заполните все поля!'
+            return jsonify({'error': 'Заполните все поля!'})
         elif self.password != password2:
-            return 'Пароли не совпадают'
+            return jsonify({'error': 'Пароли не совпадают'})
         else:
             hash_password = generate_password_hash(self.password)
             new_user = User(sign_login=self.login, sign_password=hash_password, sign_role=role)
-            app.session.add(new_user)
-            app.session.commit()
+            db.session.add(new_user)
+            try:
+                db.session.commit()
+                return jsonify({
+                    "sign_login": self.login,
+                    "password": self.password,
+                    "sign_role": role
+                })
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                return jsonify({'error': 'Произошла ошибка при добавлении в базу данных!'})
 
-class UserAPI(Resource):
+class UserAPISignIn(Resource):
     @swag_from("user_json.yml")
     def post(self):
         login = request.args.get("login")
@@ -56,25 +64,43 @@ class UserAPI(Resource):
         res = user.sign_up()
         return res
 
+class UserAPIRegistraion(Resource):
+    @swag_from("user_json_reg.yml")
+    def post(self):
+        login = request.args.get("login")
+        password = request.args.get("password")
+        password2 = request.args.get("password2")
+        role = request.args.get("role")
+        user = Users(login, password)
+        res = user.registration(password2, role)
+        return res
+
+
 class Worker:
-    def __init__(self, id_worker, name_worker, post_worker,phone_number,login):
+    def __init__(self, id_worker, name_worker, post_worker, phone_number, login):
         self.id_worker = id_worker
         self.name_worker = name_worker
         self.post_worker = post_worker
         self.phone_number = phone_number
         self.login = login
+
     def add_info(self):
         new_worker = Workers(idworker=self.id_worker, nameworker=self.name_worker,
-                                 postworker=self.post_worker, phonenumber=self.phone_number,
-                                 login=self.login)
+                             postworker=self.post_worker, phonenumber=self.phone_number,
+                             login=self.login)
 
-        app.session.add(new_worker)
+        db.session.add(new_worker)
         try:
-            app.session.commit()
-            return ''
+            db.session.commit()
+            return jsonify({"idworker": self.id_worker,
+                            "nameworker": self.name_worker,
+                            "postworker": self.post_worker,
+                            "phonenumber": self.phone_number,
+                            "login": self.login})
         except SQLAlchemyError:
-            app.session.rollback()
-            return 'Произошла ошибка при добавлении в базу данных!'
+            db.session.rollback()
+            return jsonify({'error': 'Произошла ошибка при добавлении в базу данных!'})
+
 class WorkerAPI(Resource):
     @swag_from("worker_json.yml")
     def post(self):
@@ -84,9 +110,29 @@ class WorkerAPI(Resource):
         phone_number = request.args.get("phone_number")
         login = request.args.get("login")
 
-        worker = Worker(id_worker, name_worker, post_worker,phone_number,login)
+        worker = Worker(id_worker, name_worker, post_worker, phone_number, login)
         res = worker.add_info()
         return res
+class WorkerAPIGetInfo(Resource):
+    @swag_from("worker_json_get.yml")
+    def get(self):
+        worker = Workers.query.all()
+        res = []
+
+        for i in worker:
+            idworker = str(i.idworker)
+            nameworker = str(i.nameworker)
+            postworker = str(i.postworker)
+            phonenumber = str(i.phonenumber)
+            login = str(i.login)
+            res.append({
+                "idworker": idworker,
+                "nameworker": nameworker,
+                "postworker": postworker,
+                "phonenumber": phonenumber,
+                "login": login
+            })
+        return jsonify(res)
 
 class Customer:
     def __init__(self, namecustomer, birthdaycustomer, passportdetails, login):
@@ -94,19 +140,24 @@ class Customer:
         self.birthdaycustomer = birthdaycustomer
         self.passportdetails = passportdetails
         self.login = login
+
     def add_info(self):
 
         new_customer = Customers(namecustomer=self.namecustomer,
                                  birthdaycustomer=self.birthdaycustomer, passportdetails=self.passportdetails,
                                  login=self.login)
 
-        app.session.add(new_customer)
+        db.session.add(new_customer)
         try:
-            app.session.commit()
-            return ''
+            db.session.commit()
+            return jsonify({"namecustomer": self.namecustomer,
+                            "birthdaycustomer": self.birthdaycustomer,
+                            "passportdetails": self.passportdetails,
+                            "login": self.login})
         except SQLAlchemyError:
-            app.session.rollback()
-            return 'Произошла ошибка при добавлении в базу данных!'
+            db.session.rollback()
+            return jsonify({'error': 'Произошла ошибка при добавлении в базу данных!'})
+
 class CustomerAPI(Resource):
     @swag_from("customer_json.yml")
     def post(self):
@@ -121,7 +172,7 @@ class CustomerAPI(Resource):
 
 class Contract:
     def __init__(self, login_user, login_customer, number_contract, date_contract, type_contract, period_contract,
-                     status_contract, value_contract, id_direction):
+                 status_contract, value_contract, id_direction):
         self.login_user = login_user
         self.login_customer = login_customer
         self.number_contract = number_contract
@@ -133,48 +184,78 @@ class Contract:
         self.id_direction = id_direction
 
     def add_contract(self):
-        passport_customer = app.session.query(Customers.passportdetails).filter_by(
-            login=self.login_customer).scalar_subquery()
-        id_worker = app.session.query(Workers.idworker).filter_by(login=self.login_user).scalar_subquery()
+        passport_customer = Customers.query.filter_by(login=self.login_customer).all()[0].passportdetails
+        id_worker = Workers.query.filter_by(login=self.login_user).all()[0].idworker
         new_contract = Contracts(numbercontract=self.number_contract, datecontract=self.date_contract,
                                  typecontract=self.type_contract, periodcontract=self.period_contract,
-                                 statuscontract=self.status_contract, valuecontract=self.value_contract, iddirection=self.id_direction,
+                                 statuscontract=self.status_contract, valuecontract=self.value_contract,
+                                 iddirection=self.id_direction,
                                  idworker=id_worker, customerpassport=passport_customer)
 
-        app.session.add(new_contract)
+        db.session.add(new_contract)
         try:
-            app.session.commit()
-            return 'operator_space'
+            db.session.commit()
+            return jsonify({
+                "numbercontract": self.number_contract,
+                "datecontract": self.date_contract,
+                "typecontract": self.type_contract,
+                "periodcontract": self.period_contract,
+                "statuscontract": self.status_contract,
+                "valuecontract": self.value_contract,
+                "customerpassport": passport_customer,
+                "iddirection": self.id_direction,
+                "idworker": id_worker
+            })
         except SQLAlchemyError:
-            app.session.rollback()
-            return 'Произошла ошибка при добавлении в базу данных!'
+            db.session.rollback()
+            return jsonify({'error': 'Произошла ошибка при добавлении в базу данных!'})
 
-    def find_record(self, number_contract):
-        rec = Contracts.query.filter_by(numbercontract=number_contract).all()
-        return rec
 
 class ContractAPIGetInfo(Resource):
     @swag_from("contract_json_get.yml")
     def get(self):
         contracts = Contracts.query.all()
+        res = []
+        for i in contracts:
+            numbercontract = str(i.numbercontract)
+            datecontract = str(i.datecontract)
+            typecontract = str(i.typecontract)
+            periodcontract = str(i.periodcontract)
+            statuscontract = str(i.statuscontract)
+            valuecontract = str(i.valuecontract)
+            customerpassport = str(i.customerpassport)
+            iddirection = str(i.iddirection)
+            idworker = str(i.idworker)
+            res.append({
+                "numbercontract": numbercontract,
+                "datecontract": datecontract,
+                "typecontract": typecontract,
+                "periodcontract": periodcontract,
+                "statuscontract": statuscontract,
+                "valuecontract": valuecontract,
+                "customerpassport": customerpassport,
+                "iddirection": iddirection,
+                "idworker": idworker
+            })
+        return jsonify(res)
 
 class ContractAPIAddContract(Resource):
     @swag_from("contract_json_post.yml")
     def post(self):
-        login_user =  request.args.get("login_user")
-        login_customer =  request.args.get("login_customer")
-        number_contract =  request.args.get("number_contract")
-        date_contract =  request.args.get("date_contract")
-        type_contract =  request.args.get("type_contract")
-        period_contract =  request.args.get("period_contract")
-        status_contract =  request.args.get("status_contract")
-        value_contract =  request.args.get("value_contract")
-        id_direction =  request.args.get("id_direction")
+        login_user = request.args.get("login_user")
+        login_customer = request.args.get("login_customer")
+        number_contract = request.args.get("number_contract")
+        date_contract = request.args.get("date_contract")
+        type_contract = request.args.get("type_contract")
+        period_contract = request.args.get("period_contract")
+        status_contract = request.args.get("status_contract")
+        value_contract = request.args.get("value_contract")
+        id_direction = request.args.get("id_direction")
 
         contract = Contract(login_user, login_customer, number_contract, date_contract, type_contract, period_contract,
-                     status_contract, value_contract, id_direction)
+                            status_contract, value_contract, id_direction)
 
-        res = contract.add_info()
+        res = contract.add_contract()
         return res
 
 class ContractAPISetStatus(Resource):
@@ -185,18 +266,67 @@ class ContractAPISetStatus(Resource):
         status = request.args.get("status")
 
         if (start > end):
-            return 'Начало периода не может быть позже окончания периода!'
+            return jsonify({'error': 'Начало периода не может быть позже окончания периода!'})
         else:
-            app.session.execute(sqlalchemy.text("CALL update_status(:param1, :param2, :param3)"),
+            db.session.execute(sqlalchemy.text("CALL update_status(:param1, :param2, :param3)"),
                                 {"param1": status, "param2": start, "param3": end})
             try:
-                app.session.commit()
-                return jsonify({
-                    "status": status
-                })
+                db.session.commit()
+                contracts = Contracts.query.filter(Contracts.datecontract>=start,Contracts.datecontract<=end).all()
+                res = []
+                for i in contracts:
+                    numbercontract = str(i.numbercontract)
+                    datecontract = str(i.datecontract)
+                    typecontract = str(i.typecontract)
+                    periodcontract = str(i.periodcontract)
+                    statuscontract = str(i.statuscontract)
+                    valuecontract = str(i.valuecontract)
+                    customerpassport = str(i.customerpassport)
+                    iddirection = str(i.iddirection)
+                    idworker = str(i.idworker)
+                    res.append({
+                        "numbercontract": numbercontract,
+                        "datecontract": datecontract,
+                        "typecontract": typecontract,
+                        "periodcontract": periodcontract,
+                        "statuscontract": statuscontract,
+                        "valuecontract": valuecontract,
+                        "customerpassport": customerpassport,
+                        "iddirection": iddirection,
+                        "idworker": idworker
+                    })
+                return jsonify(res)
             except SQLAlchemyError:
-                return 'Произошла ошибка на стороне базы данных!'
+                return jsonify({'error:': 'Произошла ошибка на стороне базы данных!'})
 
+class ContractAPIFindRecord(Resource):
+    @swag_from("contract_json_get_find_record.yml")
+    def get(self):
+        number_contract = request.args.get("number_contract")
+        contracts = Contracts.query.filter_by(numbercontract=number_contract).all()
+        res = []
+        for i in contracts:
+            numbercontract = str(i.numbercontract)
+            datecontract = str(i.datecontract)
+            typecontract = str(i.typecontract)
+            periodcontract = str(i.periodcontract)
+            statuscontract = str(i.statuscontract)
+            valuecontract = str(i.valuecontract)
+            customerpassport = str(i.customerpassport)
+            iddirection = str(i.iddirection)
+            idworker = str(i.idworker)
+            res.append({
+                "numbercontract": numbercontract,
+                "datecontract": datecontract,
+                "typecontract": typecontract,
+                "periodcontract": periodcontract,
+                "statuscontract": statuscontract,
+                "valuecontract": valuecontract,
+                "customerpassport": customerpassport,
+                "iddirection": iddirection,
+                "idworker": idworker
+            })
+        return jsonify(res)
 
 
 class AddPay:
@@ -212,8 +342,9 @@ class AddPay:
         try:
             app.session.add(new_pay)
             app.session.commit()
-        except:
-            return "Произошла ошибка!"
+        except SQLAlchemyError:
+            app.session.rollback()
+            return jsonify({'error': 'Произошла ошибка при добавлении в базу данных!'})
 
 class PayAPIAddPay(Resource):
 
@@ -236,16 +367,53 @@ class PayAPIGetInfo(Resource):
     @swag_from("pay_json_get.yml")
     def get(self):
         pay = Pay.query.all()
+        res = []
 
+        for i in pay:
+            accountnumber = str(i.accountnumber)
+            paymentmethod = str(i.paymentmethod)
+            paymentdate = str(i.paymentdate)
+            numbercontract = str(i.numbercontract)
+            res.append({
+                "accountnumber": accountnumber,
+                "paymentmethod": paymentmethod,
+                "paymentdate": paymentdate,
+                "numbercontract": numbercontract
+            })
+        return jsonify(res)
+
+
+class DirectionAPIGetInfo(Resource):
+    @swag_from("direction_json_get.yml")
+    def get(self):
+        pay = Directions.query.all()
+        res = []
+
+        for i in pay:
+            typedirection = str(i.typedirection)
+            iddirection = str(i.iddirection)
+            namedirection = str(i.namedirection)
+            idexecutor = str(i.idexecutor)
+            res.append({
+                "typedirection": typedirection,
+                "iddirection": iddirection,
+                "namedirection": namedirection,
+                "idexecutor": idexecutor
+            })
+        return jsonify(res)
 
 # Api resource routing
-api.add_resource(UserAPI, '/user/sign_in')
+api.add_resource(UserAPISignIn, '/user/sign_in')
+api.add_resource(UserAPIRegistraion, '/user/registraion')
 api.add_resource(ContractAPIGetInfo, '/contract/get_info')
 api.add_resource(ContractAPIAddContract, '/contract/add_contract')
 api.add_resource(ContractAPISetStatus, '/contract/set_status')
+api.add_resource(ContractAPIFindRecord, '/contract/find_record')
 api.add_resource(PayAPIGetInfo, '/pay/get_info')
 api.add_resource(PayAPIAddPay, '/pay/add_pay')
 api.add_resource(WorkerAPI, '/worker/add_info')
+api.add_resource(WorkerAPIGetInfo, '/worker/get_info')
 api.add_resource(CustomerAPI, '/customer/add_info')
+api.add_resource(DirectionAPIGetInfo, '/direction/get_info')
 if __name__ == "__main__":
     app.run(debug=True)
--- university/models.py
+++ university/models.py
@@ -1,77 +1,91 @@
 from flask_login import UserMixin
-
+from flask import jsonify
 from university import db
 
 
 class User(db.Model, UserMixin):
-    __tablename__= 'users'
-    sign_login =  db.Column(db.String(100), primary_key = True, nullable = True, unique = True)
+    __tablename__ = 'users'
+    sign_login = db.Column(db.String(100), primary_key=True, nullable=True, unique=True)
     sign_password = db.Column(db.String(100), nullable=True)
     sign_role = db.Column(db.Integer, nullable=True)
 
     def get_id(self):
         return (self.sign_login)
 
+
 class Customers(db.Model):
     __tablename__ = 'customers'
-    namecustomer = db.Column(db.String(256), nullable = True)
-    birthdaycustomer = db.Column(db.Date, nullable = True)
-    passportdetails = db.Column(db.Integer, nullable = True, primary_key= True)
-    login = db.Column(db.String(256), db.ForeignKey('users.login'), nullable = True)
+    namecustomer = db.Column(db.String(256), nullable=True)
+    birthdaycustomer = db.Column(db.Date, nullable=True)
+    passportdetails = db.Column(db.Integer, nullable=True, primary_key=True)
+    login = db.Column(db.String(256), db.ForeignKey('users.sign_login'), nullable=True)
 
     def __repr__(self):
-        return '<Directions %r>' % self.passportdetails
+        return '{namecustomer: %r,  birthdaycustomer: %r, passportdetails: %r, login: %r}' % (
+            self.namecustomer, self.birthdaycustomer, self.passportdetails, self.login)
+
 
 class Workers(db.Model):
     __tablename__ = 'workers'
     idworker = db.Column(db.Integer, primary_key=True, nullable=True)
-    nameworker = db.Column(db.String(256), nullable = True)
-    postworker = db.Column(db.String(256), nullable = True)
-    phonenumber = db.Column(db.String(256), nullable = True)
-    login = db.Column(db.String(256), db.ForeignKey('users.login'), nullable = True)
+    nameworker = db.Column(db.String(256), nullable=True)
+    postworker = db.Column(db.String(256), nullable=True)
+    phonenumber = db.Column(db.String(256), nullable=True)
+    login = db.Column(db.String(256), db.ForeignKey('users.sign_login'), nullable=True)
 
     def __repr__(self):
-        return '<Directions %r>' % self.idworker
+        return '{idworker: %r,  nameworker: %r, postworker: %r, phonenumber: %r, login: %r}' % (
+            self.idworker, self.nameworker, self.postworker, self.phonenumber, self.login)
 
 
 class Executors(db.Model):
     __tablename__ = 'executors'
-    faculty = db.Column(db.String(256), nullable = True)
-    department = db.Column(db.Integer, nullable = True)
-    idexecutor = db.Column(db.Integer,nullable = True, primary_key= True)
+    faculty = db.Column(db.String(256), nullable=True)
+    department = db.Column(db.Integer, nullable=True)
+    idexecutor = db.Column(db.Integer, nullable=True, primary_key=True)
 
     def __repr__(self):
-        return '<Directions %r>' % self.idexecutor
+        return '{faculty: %r,  department: %r, idexecutor: %r}' % (
+            self.faculty, self.department, self.idexecutor)
+
 
 class Directions(db.Model):
     __tablename__ = 'directions'
-    typedirection= db.Column(db.String(256), nullable = True)
-    iddirection = db.Column(db.String(256), primary_key= True, nullable = True)
-    namedirection = db.Column(db.String(256), nullable = True)
-    idexecutor = db.Column(db.Integer, db.ForeignKey('executors.idexecutor'), nullable = True)
+    typedirection = db.Column(db.String(256), nullable=True)
+    iddirection = db.Column(db.String(256), primary_key=True, nullable=True)
+    namedirection = db.Column(db.String(256), nullable=True)
+    idexecutor = db.Column(db.Integer, db.ForeignKey('executors.idexecutor'), nullable=True)
 
     def __repr__(self):
-        return '<Directions %r>' % self.iddirection
+        return '{typedirection: %r,  iddirection: %r, namedirection: %r, idexecutor: %r}' % (
+            self.typedirection, self.iddirection, self.namedirection, self.idexecutor)
+
+
 class Contracts(db.Model):
     __tablename__ = 'contracts'
-    numbercontract = db.Column(db.Integer, primary_key= True, nullable = True)
-    datecontract = db.Column(db.Date, nullable = True)
-    typecontract = db.Column(db.String(256), nullable = True)
-    periodcontract = db.Column(db.String(256), nullable = True)
-    statuscontract = db.Column(db.String(256), nullable = True)
-    valuecontract = db.Column(db.Integer, nullable = True)
-    customerpassport = db.Column(db.Integer, db.ForeignKey('customers.passportdetails'), nullable = True)
-    iddirection = db.Column(db.String(256), db.ForeignKey('directions.iddirection'), nullable = True)
-    idworker = db.Column(db.Integer, db.ForeignKey('workers.idworker'), nullable = True)
+    numbercontract = db.Column(db.Integer, primary_key=True, nullable=True)
+    datecontract = db.Column(db.Date, nullable=True)
+    typecontract = db.Column(db.String(256), nullable=True)
+    periodcontract = db.Column(db.String(256), nullable=True)
+    statuscontract = db.Column(db.String(256), nullable=True)
+    valuecontract = db.Column(db.Integer, nullable=True)
+    customerpassport = db.Column(db.Integer, db.ForeignKey('customers.passportdetails'), nullable=True)
+    iddirection = db.Column(db.String(256), db.ForeignKey('directions.iddirection'), nullable=True)
+    idworker = db.Column(db.Integer, db.ForeignKey('workers.idworker'), nullable=True)
 
     def __repr__(self):
-        return '<Directions %r>' % self.numbercontract
+        return '{numbercontract: %r,  datecontract: %r, typecontract: %r, periodcontract: %r, statuscontract: %r, valuecontract: %r, customerpassport: %r, iddirection: %r, idworker: %r}' % (
+            self.numbercontract, self.datecontract, self.typecontract, self.statuscontract, self.valuecontract,
+            self.customerpassport, self.iddirection, self.idworker)
+
+
 class Pay(db.Model):
     __tablename__ = 'pay'
-    accountnumber = db.Column(db.Integer, primary_key= True, nullable = True)
-    paymentdate= db.Column(db.Date, nullable = True)
-    paymentmethod = db.Column(db.String(256), nullable = True)
-    numbercontract = db.Column(db.Integer, db.ForeignKey('contracts.numbercontract'), nullable = True)
+    accountnumber = db.Column(db.Integer, primary_key=True, nullable=True)
+    paymentdate = db.Column(db.Date, nullable=True)
+    paymentmethod = db.Column(db.String(256), nullable=True)
+    numbercontract = db.Column(db.Integer, db.ForeignKey('contracts.numbercontract'), nullable=True)
 
     def __repr__(self):
-        return '<Directions %r>' % self.accountnumber
+        return '{accountnumber: %r,  paymentdate: %r, paymentmethod: %r, numbercontract: %r}' % (
+        self.accountnumber, self.paymentdate, self.paymentmethod, self.numbercontract)
